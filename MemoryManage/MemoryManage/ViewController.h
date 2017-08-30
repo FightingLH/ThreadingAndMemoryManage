50d53b3cb07bb3ec95f1aee8fe03d8333be15f8b
lh <feeyo@feeyodeMac-mini.local>
2017-08-30 17:29:22 +0800

    I updated the code again haha

---
+++ MemoryManage/MemoryManage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryManage
+//
+//  Created by feeyo on 2017/8/30.
+//  Copyright © 2017年 feeyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
