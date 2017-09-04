361c39aef9e7eed01e28e8a5044d2777a085f8c4
lh <feeyo@feeyodeMac-mini.local>
2017-09-04 16:07:50 +0800

    u r a pig

--- MemoryManage/MemoryManage/ViewController.m
+++ MemoryManage/MemoryManage/ViewController.m
@@ -12,7 +12,7 @@ struct test
 {
     NSMutableArray __unsafe_unretained *array;
     NSString       __unsafe_unretained *str;
-};
+};//编译不属于内存管理对象，因此使用时应该注意
 
 @interface ViewController ()
 
@@ -23,7 +23,15 @@ struct test
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-   
+//    属性：
+//    属性与所有权的对应关系
+//    assign    __unsafe_unretained
+//    copy      __strong
+//    retain    __strong
+//    strong    __strong
+//    __unsafe_unretained __unsafe_unretained
+//    weak      __weak
+    
 }
 
 
